30201d3a02dbe59ba98c402eedf1b3355730166c
milan <milanodd@gmail.com>
2023-05-08 16:08:56 -0400

    NewClass added and existing classes modified.

---
+++ src/test/java/orgPack/NewClass.java
@@ -0,0 +1,4 @@
+package orgPack;
+
+public class NewClass {
+}
---
+++ src/test/java/orgPack/Test2Class.java
@@ -0,0 +1,10 @@
+package orgPack;
+
+public class Test2Class {
+
+    public void test2(){
+
+        System.out.println("Test 222");
+    }
+
+}
